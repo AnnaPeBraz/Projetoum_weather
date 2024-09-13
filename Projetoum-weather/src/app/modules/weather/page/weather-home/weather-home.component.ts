2802a6685c7e11ece398b9dbde98aaf6400f2741
unknown <annap.0103@gmail.com>
2024-09-13 19:00:51 -0300

    consumindo o servico da api

--- Projetoum-weather/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ Projetoum-weather/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'Sao Paulo';
+  weatherDatas!: WeatherDatas;
 
+  constructor(private weatherService: WeatherService){}
+  
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+      },
+      error: (err) => console.log(err),
+    })
+  }
+  
 }
